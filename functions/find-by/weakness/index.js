7796ad337e7e9b152496f74b631c4a14a20dc21c
ikoral <ilhankoral@outlook.com>
2019-12-14 16:00:38 +0100

    weakness functon created.
    First I have checked if the pokemon exist by given weakness. Then I have created empty array. After that I checke each element and push required field to newly created array. If filtered array empty new created array will be also empty.

--- functions/find-by/weakness/index.js
+++ functions/find-by/weakness/index.js
@@ -1,4 +1,14 @@
 const weakness = (pokeArray, type) => {
+//check if we have any pokemon with this weakness
+const resultArrayFull = pokeArray.filter(element =>
+    element["weaknesses"].find(item => item == type));
+  
+//return array include name and num
+const resultArrayExpected = [];
+resultArrayFull.forEach(element => {
+    resultArrayExpected.push({"name": element.name, "num": element.num})
+});
+return resultArrayExpected;
 
 };
 
