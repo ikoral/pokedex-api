bfa1e36c162690b4bf19fe29a6feabfd63a06bf1
ilhan koral <ilhankoral@outlook.com>
2019-12-14 02:45:28 +0100

    evolution functon created.
    First I have checked if the pokemon exist by given name. if it is not empty array returned. Then pokemon checkedif it has prev and next evolution. First prev, then itself and at the and next evolution pushed new created result array. Result array returned.

--- functions/evolutions-of/index.js
+++ functions/evolutions-of/index.js
@@ -1,5 +1,25 @@
 const evolutionOf = (pokeArray, name) => {
+    //find pokemon by given name
+    const pokemon = pokeArray.find(element => element.name === name);
+    //if there is no pokemon then return false.
+    if (!pokemon){
+        return [];
+    }
 
+    const resultArray = [];
+
+    //"prev_evolution"
+    if ("prev_evolution" in pokemon){
+        resultArray.push(...pokemon.prev_evolution);
+    }
+    //pokemon itself
+    resultArray.push({"num":pokemon.num, "name": pokemon.name});
+    //"next_evolution"
+    if ("next_evolution" in pokemon){
+        resultArray.push(...pokemon.next_evolution);
+    };
+
+    return resultArray;
 };
 
 module.exports = evolutionOf;
