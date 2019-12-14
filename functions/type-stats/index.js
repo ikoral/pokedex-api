3e4dafc91e4597107a5ed3d11f1eae5c27a59b58
ikoral <ilhankoral@outlook.com>
2019-12-14 18:38:29 +0100

    typeStats function is created
    I have used my current function to calculate how many types we have and have many weakness this pokemon

--- functions/type-stats/index.js
+++ functions/type-stats/index.js
@@ -1,5 +1,14 @@
-const typeStats = (pokeArray, type) => {
+const weakness = require("../find-by/weakness/index");
+const types = require("../find-by/type/index");
 
+
+const typeStats = (pokeArray, type) => {
+    
+    return {
+        "typeCount": types(pokeArray,type).length,
+        "typeName": type,
+        "weaknessCount": weakness(pokeArray,type).length
+    };
 };
 
 module.exports = typeStats;
