7e1b6055dde87bee5262a1f4bf66e7dfc514c8a8
ikoral <ilhankoral@outlook.com>
2019-12-15 01:35:12 +0100

    valuesForKey Function is created
    Now according to given value first array filtered. Then mapped only related field(key) I have used first ES6 features to eliminate duplicate records in array but it didnt work. Then I use Vanilla JS object rules. Object can have unique keys. I first added all records to object then push again array

--- functions/values-for-key/index.js
+++ functions/values-for-key/index.js
@@ -11,7 +11,34 @@ const areTheSame = (first, second) => {
 }
 
 const valuesForKey = (pokeArray, key) => {
+  
+  //first map array for given value
+  const filteredArray = pokeArray.filter(item => Object.keys(item).includes(key));
+  const mappedArray = filteredArray.map(item => item[key]);
 
+  /*****remove duplication ES& new feature*****doesn't pass all test
+  //const removeDuplicateArray = [...new Set(mappedArray)];
+  //const removeDuplicate = mappedArray.filter((item,index) => 
+  //mappedArray.indexOf(item) === index);
+
+
+  /*****MY SOLUTIONS***USE OBJ UNIQUE KEY FEATURE*******/
+  let library = {};
+    let removeDuplicateArray = [];
+    for (let i =0;i<mappedArray.length;i++){
+      
+      if (!(mappedArray[i] in library)){
+        removeDuplicateArray.push(mappedArray[i]);
+        library[mappedArray[i]] = true;
+      }
+    }
+    
+  return removeDuplicateArray;
 };
 
 module.exports = valuesForKey;
+
+
+// if (value === "null"){
+//   value = JSON.parse(value);
+// }
